e90d9c04fdd4f012473ce7fa6d01b4d41b6d5837
mecarm <melecarmine99@gmail.com>
2022-12-15 16:24:00 +0100

    es completed

--- api/index.php
+++ api/index.php
@@ -3,7 +3,27 @@
 include_once __DIR__ . '/../db/index.php';
 // var_dump(__DIR__);
 
+$genereFiltrato = [];
+
+//chiede all'url se c'è un parametro definito e se c'è un parametro genere con del valore all'interno
+if( !empty($_GET) && !empty($_GET['genre'] ) ){
+
+  foreach( $database as $elem ){
+
+    if( $elem['genre'] == $_GET['genre'] ){
+      //se il tipo del genere corrisponde al genere scritto nel parametro url "genere" pusho l'elemento dell'genere filtrato
+      $genereFiltrato[] = $elem;
+    }
+  }
+
+  //Se non c'è nessun parametro nell'url e se ci sono sono vuoti
+} else {
+  //associazione dell'array genereFiltrato con l'intero array di elementi
+      $genereFiltrato = $database;
+}
+
 header('Content-type: application/json');
 
-echo json_encode( $database );
+echo json_encode( $genereFiltrato );
+
 ?>
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,29 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        dischi : [],
-    },
-    mounted() {
-      axios.get('./api/index.php')
-        .then ( (response) => {
-            this.dischi = response.data
-            
-        })
-    },
-    beforeUpdate() {
-  
-    },
-    methods: {
+  el: "#app",
+  data: {
+    dischi: [],
+    generi: [],
+    genereSelezionato: "",
+  },
+  mounted() {
+    axios.get("./api/index.php").then((response) => {
+      this.dischi = response.data;
 
-    }
-  })
+      //ciclo per estrarre e filtrare i generi
+      this.dischi.forEach((element) => {
+        //condizione per filtrare i generi uguali e pusharli solo una volta
+        if (!this.generi.includes(element.genre)) {
+          this.generi.push(element.genre);
+        }
+      });
+    });
+  },
+  beforeUpdate() {},
+  methods: {
+    genereSelected() {
+      axios.get("./api/index.php?genre=" + this.genereSelezionato).then((response) => {
+        this.dischi = response.data;
+      });
+    },
+  },
+});
